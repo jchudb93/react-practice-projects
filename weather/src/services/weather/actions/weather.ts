0a8e0d7aa8cc86158663c7365842d78e3b02c257
José Chu <jchudb93@gmail.com>
2018-07-10 23:48:44 -0500

    reducer

--- weather/src/services/weather/actions/weather.ts
+++ weather/src/services/weather/actions/weather.ts
@@ -1,17 +1,18 @@
 import axios from 'axios'
+
 const API_KEY = 'b9477d1b205c2a0697e56d6ff054f4cc';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export const fetchWeather = city:string => {
+export const fetchWeather = (city: string) => {
 
   const url = `${ROOT_URL}&q=${city},us`;
+  
   const request= axios.get(url);
 
-  console.log(request);
-
-  return (
-    type: 'FETCH_WEATHER',
+  return(    {
     payload: request,
-  );
+    type: 'FETCH_WEATHER'
+  });
 } 
