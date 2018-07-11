f21a13500056864e90c6b5ad3e0026cc54dd6437
José Chu <jchudb93@gmail.com>
2018-07-10 23:13:16 -0500

    requests

---
+++ weather/src/scenes/home/components/SearchBar.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+//  import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+
+// import { fetchWeather } from '../../../services/actions/weather';
+
+
+
+class SearchBar extends React.Component<any, any> {
+
+  public render(){
+    return(<div>
+      <a>holi</a>
+    </div>);
+  }
+}
+
+export default SearchBar;
